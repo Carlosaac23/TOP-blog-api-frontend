c65ab9498c1ef5ca52c4d303361c7fc83cbec0ba
Carlos <carlosaac232001@gmail.com>
2026-03-26 11:42:53 -0500

    chore: update user schema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -2,14 +2,14 @@ import z from 'zod';
 
 export const CreateUserSchema = z.object({
   firstName: z.string().min(1, 'First name is required'),
-  lastName: z.string().min(1, 'Last name is required'),
+  lastName: z.string().optional(),
   username: z.string().min(3, 'Username must be at least 3 characters long'),
   email: z.email('Invalid email address'),
   password: z.string().min(12, 'Password must be at least 12 characters long'),
   birthDate: z.string().refine(date => {
     const parsedDate = new Date(date);
     return !isNaN(parsedDate.getTime());
-  }, 'Invalid date format'),
+  }, 'You must provide a valid date'),
   role: z.enum(['user', 'writer']),
 });
 

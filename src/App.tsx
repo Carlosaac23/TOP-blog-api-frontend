b354dfe3a7e940c2e5970ca7e825412c411ad180
Carlos <carlosaac232001@gmail.com>
2026-03-26 14:10:27 -0500

    chore: import new layout and and update pages names

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import { Route, Routes } from 'react-router-dom';
 
 import SignInForm from '@/components/SignInForm';
 import SignUpForm from '@/components/SignUpForm';
+import PrivateLayout from '@/layouts/PrivateLayout';
 import PublicLayout from '@/layouts/PublicLayout';
-import Hero from '@/pages/Home';
+import PublicHome from '@/pages/Home';
+import PrivateHome from '@/pages/private/Home';
 
 export default function App() {
   return (
     <main>
       <Routes>
+        {/* Public Routes */}
         <Route path='/' element={<PublicLayout />}>
-          <Route index element={<Hero />} />
+          <Route index element={<PublicHome />} />
           <Route path='sign-up' element={<SignUpForm />} />
           <Route path='sign-in' element={<SignInForm />} />
+        </Route>
+
+        {/* Private Routes */}
+        <Route path='/profile' element={<PrivateLayout />}>
+          <Route index element={<PrivateHome />} />
         </Route>
       </Routes>
     </main>

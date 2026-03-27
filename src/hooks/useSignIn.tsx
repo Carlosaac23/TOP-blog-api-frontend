e0a01daeda29e4bafb75b7815a17d2ab8792a9db
Carlos <carlosaac232001@gmail.com>
2026-03-27 16:42:46 -0500

    chore: use signIn logic from AuthProvider

--- src/hooks/useSignIn.tsx
+++ src/hooks/useSignIn.tsx
@@ -3,15 +3,17 @@ import { useNavigate } from 'react-router-dom';
 import type { LoginUserInput } from '@/schemas/userSchema';
 
 import { axiosClient } from '@/config/axios';
+import { useAuth } from '@/context/AuthProvider';
 export function useSignIn() {
   const navigate = useNavigate();
+  const { signIn } = useAuth();
 
   const handleSubmit = async (values: LoginUserInput) => {
     const { data } = await axiosClient.post('/', values);
     const { token } = data;
 
-    localStorage.setItem('bloggering_token', token);
-    navigate('/profile');
+    await signIn(token);
+    navigate('/home');
   };
 
   return { handleSubmit };

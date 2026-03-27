1601587e97be9fd9be54279f044ceae5a255ba35
Carlos <carlosaac232001@gmail.com>
2026-03-27 16:35:51 -0500

    chore: improve rendering by showing a spinner while loading

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -3,22 +3,29 @@ import { Navigate } from 'react-router-dom';
 
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import { Spinner } from '@/components/ui/spinner';
 import { useAuth } from '@/context/AuthProvider';
 
 export default function PrivateLayout() {
-  const { auth } = useAuth();
+  const { auth, loadingAuth } = useAuth();
+
+  if (loadingAuth) {
+    return (
+      <main className='flex min-h-screen flex-col items-center justify-center'>
+        <Spinner />
+      </main>
+    );
+  }
+
+  if (!auth?.sub) {
+    return <Navigate to='/' replace />;
+  }
 
   return (
-    <main>
-      {auth?.sub ? (
-        <main>
-          <Header />
-          <Outlet />
-          <Footer />
-        </main>
-      ) : (
-        <Navigate to='/' />
-      )}
+    <main className='flex min-h-screen flex-col'>
+      <Header />
+      <Outlet />
+      <Footer />
     </main>
   );
 }

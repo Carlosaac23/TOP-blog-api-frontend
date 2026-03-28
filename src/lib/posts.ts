26383222ae7cfad65584c3e48d71b60c71ece663
Carlos <carlosaac232001@gmail.com>
2026-03-27 20:48:44 -0500

    chore: create function to order posts by date desc

---
+++ src/lib/posts.ts
@@ -0,0 +1,6 @@
+import type { Post } from '@/types';
+export function sortPostsByDateDesc(posts: Post[]) {
+  return [...posts].sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  );
+}

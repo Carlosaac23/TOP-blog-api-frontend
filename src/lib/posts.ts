05099bc918fdb3052040457a63fbd029043dba4c
Carlos <carlosaac232001@gmail.com>
2026-03-27 20:55:23 -0500

    chore: format code

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,4 +1,5 @@
 import type { Post } from '@/types';
+
 export function sortPostsByDateDesc(posts: Post[]) {
   return [...posts].sort(
     (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()

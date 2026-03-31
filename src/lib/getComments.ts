d8a8d47c126a2cecd3c0a0ca05bef8eb89cd50f2
Carlos <carlosaac232001@gmail.com>
2026-03-30 21:10:38 -0500

    chore: improve naming and type safety

--- src/lib/getComments.ts
+++ src/lib/getComments.ts
@@ -1,7 +1,7 @@
 import type { Comment } from '@/types';
 
 import { axiosClient } from '@/config/axios';
-export async function getComments(postId: string): Promise<Comment[]> {
+export async function getCommentsByPost(postId: string): Promise<Comment[]> {
   const { data } = await axiosClient(`/posts/${postId}/comments`);
   return data;
 }
--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,6 +1,8 @@
+import type { Post } from '@/types';
+
 import { axiosClient } from '@/config/axios';
 
-export async function getPosts() {
+export async function getPosts(): Promise<Post[]> {
   const { data } = await axiosClient('/posts');
   return data;
 }

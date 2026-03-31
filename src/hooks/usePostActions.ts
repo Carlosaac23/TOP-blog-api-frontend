5afaf7b6e70b740081de7e3d3cc8738f9285a7db
Carlos <carlosaac232001@gmail.com>
2026-03-30 21:11:40 -0500

    chore: improve naming and format

--- src/hooks/usePostActions.ts
+++ src/hooks/usePostActions.ts
@@ -4,10 +4,10 @@ type RefetchFn = () => Promise<void>;
 export function usePostActions(refetch: RefetchFn) {
   const { handleDelete } = useDeletePost();
 
-  async function onDelete(postId: string) {
+  async function onDeletePost(postId: string) {
     await handleDelete(postId);
     await refetch();
   }
 
-  return { onDelete };
+  return { onDeletePost };
 }
--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -7,6 +7,7 @@ export function useSignUp() {
   const handleSubmit = async (values: CreateUserInput) => {
     const rolePathUrl = values.role === 'user' ? 'users' : 'writers';
     const { data } = await axiosClient.post(`/${rolePathUrl}`, values);
+
     toast.success(data?.message);
   };
 

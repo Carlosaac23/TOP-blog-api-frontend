9365d06b45d5f5996b286f08290ddad6eb7ff288
Carlos <carlosaac232001@gmail.com>
2026-04-06 22:57:09 -0500

    chore: create function to handle refeth when updating a post

--- src/hooks/posts/usePostActions.ts
+++ src/hooks/posts/usePostActions.ts
@@ -1,13 +1,23 @@
+import type { CreatePostInput } from '@/schemas/formSchema';
+
 import { useDeletePost } from '@/hooks/posts/useDeletePost';
+import { useUpdatePost } from '@/hooks/posts/useUpdatePost';
 
 type RefetchFn = () => Promise<void>;
+
 export function usePostActions(refetch: RefetchFn) {
   const { handleDelete } = useDeletePost();
+  const { handleUpdate } = useUpdatePost();
 
   async function onDeletePost(postId: string) {
     const deleted = await handleDelete(postId);
     if (deleted) await refetch();
   }
 
-  return { onDeletePost };
+  async function onUpdatePost(postId: string, values: CreatePostInput) {
+    await handleUpdate(postId, values);
+    await refetch();
+  }
+
+  return { onDeletePost, onUpdatePost };
 }

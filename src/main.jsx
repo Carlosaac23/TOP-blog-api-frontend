9ca55bac0af4e829a839ed5b8b185618eabb4976
Carlos <carlosaac232001@gmail.com>
2026-04-07 22:05:39 -0500

    chore: start to move project to js/jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/sonner';
 import '@/index.css';
 import { AuthProvider } from '@/context/AuthProvider';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
